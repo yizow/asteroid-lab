38b3620ab937ea4b1a297e08f19d72f42a4a1836
Christine Wang <christine.wang.395@gmail.com>
2017-02-10 10:12:48 -0800

    Fixed Docker install

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 apt-get update
 
-apt-get -y install iputils-ping wondershaper speedtest-cli trickle \
-                   docker
+apt-get install -y iputils-ping wondershaper speedtest-cli trickle
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+deb https://apt.dockerproject.org/repo raspbian-jessie main
 
+apt-get update
+apt-cache policy docker-engine
+apt-get update
+apt-get install docker-engine
+service docker start
 
-usermod -aG docker $(whoami)
