b7815474faa5c2cead248b0dbc379a6b5aa7469d
Christine Wang <christine.wang.395@gmail.com>
2017-02-13 17:29:19 -0800

    Added reset and fixed ifconfig line

--- traffic_shaping.py
+++ traffic_shaping.py
@@ -2,7 +2,10 @@ import os
 
 def shape_traffic(upload_bw, download_bw):
     # Make sure txqueuelen is nonzero for Wondershaper
-    # TODO find better values than 1000 1000?
-    os.system("sudo ifconfig docker0 1000 1000")
+    # TODO find better values than 1000?
+    os.system("sudo ifconfig docker0 txqueuelen 1000")
 
     os.system("sudo wondershaper docker0 " + str(download_bw) + " " + str(upload_bw))
+
+def reset():
+    os.system("sudo wondershaper clear docker0")
